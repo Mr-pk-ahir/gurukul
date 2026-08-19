e52435a5c9c2b5b8aaa00d31277ee65ca5f5cd0f
Mr-pk-ahir <pritahir123098@gmail.com>
2026-08-19 10:06:05 +0530

    add high limits

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 1200, // kB ma - have 1200kB sudhi warning nahi aave
+    chunkSizeWarningLimit: 5200, // kB ma - have 1200kB sudhi warning nahi aave
   },
 })
