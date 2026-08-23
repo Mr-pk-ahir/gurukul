3a183b5b895cd11dca7df5427974455213d60c4a
Mr-pk-ahir <pritahir123098@gmail.com>
2026-08-23 14:10:41 +0530

    perfect set limits

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,26 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    chunkSizeWarningLimit: 50200,
+    // Limit ne standard size par set kari chhe
+    chunkSizeWarningLimit: 1500, 
+    
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // 1. React ane React-DOM mate alag file
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+              return 'react-vendor';
+            }
+            // 2. Cloudinary mate alag file (kem ke te heavy hoy chhe)
+            if (id.includes('cloudinary')) {
+              return 'cloudinary-vendor';
+            }
+            // 3. Baki na badha packages mate common file
+            return 'vendor';
+          }
+        },
+      },
+    },
   },
 })
